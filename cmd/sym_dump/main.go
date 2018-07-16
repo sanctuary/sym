b84c522b23edcf8728d4034714152a9f4c70f096
mewmew <rnd0x00@gmail.com>
2018-07-16 00:37:10 +0000

    cmd/sym_dump: remove TODO
    sym_dump is now capable of parsing both SLPS-01416
    and Easy as Pie to produce identical output as
    DUMPSYM.EXE of the Psy-Q SDK.

--- cmd/sym_dump/main.go
+++ cmd/sym_dump/main.go
@@ -12,10 +12,9 @@ func main() {
 	flag.Parse()
 	for _, path := range flag.Args() {
 		f, err := sym.ParseFile(path)
-		// TODO: move print to after error once sym package is mature.
-		fmt.Println(f)
 		if err != nil {
 			log.Fatalf("%+v", err)
 		}
+		fmt.Println(f)
 	}
 }
