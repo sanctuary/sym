9f8bcd8806960cab638efed32c9194df48680f74
Robin Eklind <rnd0x00@gmail.com>
2018-11-27 15:12:17 +0100

    cmd/sym_dump: add -merge mode and implement merging of types

--- cmd/sym_dump/main.go
+++ cmd/sym_dump/main.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/sanctuary/sym"
@@ -35,6 +37,8 @@ func main() {
 		outputDir string
 		// Output IDA scripts.
 		outputIDA bool
+		// Merge SYM files.
+		merge bool
 		// Split output into source files.
 		splitSrc bool
 		// Output C types.
@@ -43,77 +47,235 @@ func main() {
 	flag.BoolVar(&outputC, "c", false, "output C types and declarations")
 	flag.StringVar(&outputDir, "dir", dumpDir, "output directory")
 	flag.BoolVar(&outputIDA, "ida", false, "output IDA scripts")
+	flag.BoolVar(&merge, "merge", false, "merge SYM files")
 	flag.BoolVar(&splitSrc, "src", false, "split output into source files")
 	flag.BoolVar(&outputTypes, "types", false, "output C types")
 	flag.Usage = usage
 	flag.Parse()
+	if merge && outputIDA {
+		log.Fatalf("IDA output not supported in merge mode, as the scripts would be unusable.")
+	}
+
+	// Parse SYM files.
+	var ps []*csym.Parser
 	for _, path := range flag.Args() {
+		// Parse SYM file.
 		f, err := sym.ParseFile(path)
 		if err != nil {
 			log.Fatalf("%+v", err)
 		}
 		switch {
-		case outputC:
-			// Output C types and declarations.
+		case outputC, outputIDA:
+			// Parse C types and declarations.
 			p := csym.NewParser()
+			if merge {
+				ps = append(ps, p)
+			}
 			p.ParseTypes(f.Syms)
 			p.ParseDecls(f.Syms)
-			if err := initOutputDir(outputDir); err != nil {
-				log.Fatalf("%+v", errors.WithStack(err))
-			}
-			if err := dumpTypes(p, outputDir); err != nil {
-				log.Fatalf("%+v", err)
-			}
-			if splitSrc {
-				if err := dumpSourceFiles(p, outputDir); err != nil {
-					log.Fatalf("%+v", err)
-				}
-			} else {
-				if err := dumpDecls(p, outputDir); err != nil {
+			// Output once for each files if not in merge mode.
+			if !merge {
+				if err := dump(p, outputDir, outputC, outputTypes, outputIDA, splitSrc, merge); err != nil {
 					log.Fatalf("%+v", err)
 				}
 			}
 		case outputTypes:
-			// Output C types.
+			// Parse C types.
 			p := csym.NewParser()
+			if merge {
+				ps = append(ps, p)
+			}
 			p.ParseTypes(f.Syms)
-			if err := initOutputDir(outputDir); err != nil {
-				log.Fatalf("%+v", errors.WithStack(err))
-			}
-			if err := dumpTypes(p, outputDir); err != nil {
-				log.Fatalf("%+v", err)
-			}
-		case outputIDA:
-			// Output IDA scripts.
-			p := csym.NewParser()
-			p.ParseTypes(f.Syms)
-			p.ParseDecls(f.Syms)
-			if err := initOutputDir(outputDir); err != nil {
-				log.Fatalf("%+v", errors.WithStack(err))
-			}
-			if err := dumpIDAScripts(p, outputDir); err != nil {
-				log.Fatalf("%+v", err)
-			}
-			// Delete bool and __int64 types as they cause issues with IDA.
-			delete(p.Types, "bool")
-			for i, def := range p.Typedefs {
-				if v, ok := def.(*c.VarDecl); ok {
-					if v.Name == "__int64" {
-						defs := append(p.Typedefs[:i], p.Typedefs[i+1:]...)
-						p.Typedefs = defs
-						break
-					}
+			// Output once for each files if not in merge mode.
+			if !merge {
+				if err := dump(p, outputDir, outputC, outputTypes, outputIDA, splitSrc, merge); err != nil {
+					log.Fatalf("%+v", err)
 				}
-			}
-			delete(p.Types, "__int64")
-			if err := dumpTypes(p, outputDir); err != nil {
-				log.Fatalf("%+v", err)
 			}
 		default:
 			// Output in Psy-Q DUMPSYM.EXE format.
+			// Note, we never merge the Psy-Q output.
 			fmt.Print(f)
 		}
 	}
+	// Output the merge of all files if in merge mode.
+	if merge {
+		skipAddrCheck := true
+		p := pruneDuplicates(ps, skipAddrCheck)
+		if err := dump(p, outputDir, outputC, outputTypes, outputIDA, splitSrc, merge); err != nil {
+			log.Fatalf("%+v", err)
+		}
+	}
+}
+
+// pruneDuplicates prunes duplicates declarations of the parser, optionally
+// ignoring differences in address.
+func pruneDuplicates(ps []*csym.Parser, skipAddrCheck bool) *csym.Parser {
+	dst := csym.NewParser()
+	enumPresent := make(map[string]bool)
+	structPresent := make(map[string]bool)
+	unionPresent := make(map[string]bool)
+	typeDefPresent := make(map[string]bool)
+	// Add unique predeclared identifiers.
+	if def, ok := ps[0].Types["bool"]; ok {
+		dst.Types["bool"] = def
+	}
+
+	// placeholder type name to make types match even when typename differ.
+	const placeholder = "placeholder"
+	fakeEnum := 0
+	fakeStruct := 0
+	fakeUnion := 0
+	for pnum, p := range ps {
+		// Add unique enums.
+		for _, tag := range p.EnumTags {
+			t := p.Enums[tag]
+			fake := strings.Contains(t.Tag, "fake")
+			if fake {
+				t.Tag = placeholder
+			}
+			s := t.Def()
+			if fake {
+				tag = fmt.Sprintf("enum_fake_%d_%d", pnum, fakeEnum)
+				fakeEnum++
+				t.Tag = tag
+			}
+			if !enumPresent[s] {
+				if !fake {
+					tag = fmt.Sprintf("%s_dup_%d", tag, pnum)
+					t.Tag = tag
+				}
+				dst.Enums[tag] = t
+				dst.EnumTags = append(dst.EnumTags, tag)
+			}
+			enumPresent[s] = true
+		}
+		// Add unique structs.
+		for _, tag := range p.StructTags {
+			t := p.Structs[tag]
+			fake := strings.Contains(t.Tag, "fake")
+			if fake {
+				t.Tag = placeholder
+			}
+			s := t.Def()
+			if fake {
+				tag = fmt.Sprintf("struct_fake_%d_%d", pnum, fakeStruct)
+				fakeStruct++
+				t.Tag = tag
+			}
+			if !structPresent[s] {
+				if !fake {
+					if _, ok := dst.Structs[tag]; ok {
+						tag = fmt.Sprintf("%s_dup_%d", tag, pnum)
+						t.Tag = tag
+					}
+				}
+				dst.Structs[tag] = t
+				dst.StructTags = append(dst.StructTags, tag)
+			}
+			structPresent[s] = true
+		}
+		// Add unique unions.
+		for _, tag := range p.UnionTags {
+			t := p.Unions[tag]
+			fake := strings.Contains(t.Tag, "fake")
+			if fake {
+				t.Tag = placeholder
+			}
+			s := t.Def()
+			if fake {
+				tag = fmt.Sprintf("union_fake_%d_%d", pnum, fakeUnion)
+				fakeUnion++
+				t.Tag = tag
+			}
+			if !unionPresent[s] {
+				if !fake {
+					if _, ok := dst.Unions[tag]; ok {
+						tag = fmt.Sprintf("%s_dup_%d", tag, pnum)
+						t.Tag = tag
+					}
+				}
+				dst.Unions[tag] = t
+				dst.UnionTags = append(dst.UnionTags, tag)
+			}
+			unionPresent[s] = true
+		}
+		// Add unique typedefs.
+		for _, def := range p.Typedefs {
+			s := def.Def()
+			if _, ok := typeDefPresent[s]; !ok {
+				dst.Typedefs = append(dst.Typedefs, def)
+			}
+			typeDefPresent[s] = true
+		}
+	}
+	// Sort types by tag.
+	sort.Strings(dst.EnumTags)
+	sort.Strings(dst.StructTags)
+	sort.Strings(dst.UnionTags)
+	less := func(i, j int) bool {
+		ti := dst.Typedefs[i].(*c.VarDecl)
+		tj := dst.Typedefs[j].(*c.VarDecl)
+		return ti.Name < tj.Name
+	}
+	sort.Slice(dst.Typedefs, less)
+	return dst
+}
+
+// dump dumps the declarations of the parser to the given output directory, in
+// the format specified.
+func dump(p *csym.Parser, outputDir string, outputC, outputTypes, outputIDA, splitSrc, merge bool) error {
+	switch {
+	case outputC:
+		// Output C types and declarations.
+		if err := initOutputDir(outputDir); err != nil {
+			return errors.WithStack(err)
+		}
+		if err := dumpTypes(p, outputDir); err != nil {
+			return errors.WithStack(err)
+		}
+		if splitSrc {
+			if err := dumpSourceFiles(p, outputDir); err != nil {
+				return errors.WithStack(err)
+			}
+		} else {
+			if err := dumpDecls(p, outputDir); err != nil {
+				return errors.WithStack(err)
+			}
+		}
+	case outputTypes:
+		// Output C types.
+		if err := initOutputDir(outputDir); err != nil {
+			return errors.WithStack(err)
+		}
+		if err := dumpTypes(p, outputDir); err != nil {
+			return errors.WithStack(err)
+		}
+	case outputIDA:
+		// Output IDA scripts.
+		if err := initOutputDir(outputDir); err != nil {
+			return errors.WithStack(err)
+		}
+		if err := dumpIDAScripts(p, outputDir); err != nil {
+			return errors.WithStack(err)
+		}
+		// Delete bool and __int64 types as they cause issues with IDA.
+		delete(p.Types, "bool")
+		for i, def := range p.Typedefs {
+			if v, ok := def.(*c.VarDecl); ok {
+				if v.Name == "__int64" {
+					defs := append(p.Typedefs[:i], p.Typedefs[i+1:]...)
+					p.Typedefs = defs
+					break
+				}
+			}
+		}
+		delete(p.Types, "__int64")
+		if err := dumpTypes(p, outputDir); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
 }
 
 // initOutputDir initializes the output directory.
